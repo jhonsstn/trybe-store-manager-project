384ef9554dc935146e4463c0f5d4d26a4fe05aee
Jhon Santana <jhonsstn@gmail.com>
2022-06-30 18:48:50 -0300

    feat(ProductModel): ensure create a product if createProduct is called

--- models/product-model.js
+++ models/product-model.js
@@ -2,15 +2,21 @@ const db = require('./connection');
 
 const ProductModel = {
   async getProduct(id) {
-    const [product] = await db.query(
-      `SELECT * FROM StoreManager.products WHERE id = ${id}`,
-    );
+    const sqlQuery = 'SELECT * FROM StoreManager.products WHERE id = ?';
+    const [product] = await db.query(sqlQuery, [id]);
 
     return product[0];
   },
   async getProducts() {
-    const [product] = await db.query('SELECT * FROM StoreManager.products');
+    const sqlQuery = 'SELECT * FROM StoreManager.products';
+    const [product] = await db.query(sqlQuery);
     return product;
+  },
+
+  async createProduct(product) {
+    const sqlQuery = 'INSERT INTO StoreManager.products (name) VALUES (?)';
+    const [{ insertId }] = await db.query(sqlQuery, [product.name]);
+    return insertId;
   },
 };
 
