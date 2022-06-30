08a7ba4fd346f7ab94eeff1bf0804cacbd9d3331
Jhon Santana <jhonsstn@gmail.com>
2022-06-30 18:17:32 -0300

    fix(ProductModel): add db name to mysql queries

--- models/product-model.js
+++ models/product-model.js
@@ -2,11 +2,14 @@ const db = require('./connection');
 
 const ProductModel = {
   async getProduct(id) {
-    const [product] = await db.query(`SELECT * FROM products WHERE id = ${id}`);
+    const [product] = await db.query(
+      `SELECT * FROM StoreManager.products WHERE id = ${id}`,
+    );
+
     return product[0];
   },
   async getProducts() {
-    const [product] = await db.query('SELECT * FROM products');
+    const [product] = await db.query('SELECT * FROM StoreManager.products');
     return product;
   },
 };
