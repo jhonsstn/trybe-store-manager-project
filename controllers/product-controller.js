d35cfbc2c3985aac00b0b32aee27dc4b5576c6d7
Jhon Santana <jhonsstn@gmail.com>
2022-06-30 19:09:02 -0300

    feat(ProductController): ensure create a product if createProduct is called

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -9,6 +9,11 @@ const ProductController = {
     const products = await ProductService.getProducts();
     res.status(200).json(products);
   },
+
+  async createProduct(req, res) {
+    const productId = await ProductService.createProduct(req.body);
+    res.status(201).json({ id: productId, ...req.body });
+  },
 };
 
 module.exports = ProductController;
