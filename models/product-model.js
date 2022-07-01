2d800be498a83d4506adc21793f96557b191a1dc
Jhon Santana <jhonsstn@gmail.com>
2022-07-01 19:59:53 -0300

    feat(SaleModel): ensure returns sales list if calls getSale/getSales

--- models/product-model.js
+++ models/product-model.js
@@ -2,25 +2,25 @@ const db = require('./connection');
 
 const ProductModel = {
   getProduct: async (id) => {
-    const sqlQuery = 'SELECT * FROM StoreManager.products WHERE id = ?';
+    const sqlQuery = 'SELECT * FROM StoreManager.products WHERE id = ?;';
     const [product] = await db.query(sqlQuery, [id]);
 
     return product[0];
   },
   getProducts: async () => {
     const sqlQuery = 'SELECT * FROM StoreManager.products';
-    const [product] = await db.query(sqlQuery);
-    return product;
+    const [products] = await db.query(sqlQuery);
+    return products;
   },
 
   createProduct: async (product) => {
-    const sqlQuery = 'INSERT INTO StoreManager.products (name) VALUES (?)';
+    const sqlQuery = 'INSERT INTO StoreManager.products (name) VALUES (?);';
     const [{ insertId }] = await db.query(sqlQuery, [product.name]);
     return insertId;
   },
 
   productExists: async (productId) => {
-    const sqlQuery = 'SELECT 1 FROM StoreManager.products WHERE id = ?';
+    const sqlQuery = 'SELECT 1 FROM StoreManager.products WHERE id = ?;';
     const [[product]] = await db.query(sqlQuery, [productId]);
     return !!product;
   },
--- models/sale-model.js
+++ models/sale-model.js
@@ -2,16 +2,43 @@ const db = require('./connection');
 
 const SaleModel = {
   createSale: async () => {
-    const sqlQuery = 'INSERT INTO StoreManager.sales (date) VALUES (NOW())';
+    const sqlQuery = 'INSERT INTO StoreManager.sales (date) VALUES (NOW());';
     const [{ insertId }] = await db.query(sqlQuery);
 
     return insertId;
   },
 
   createProductSales: async (saleId, product) => {
-    const sqlQuery = `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
-    VALUES (?, ?, ?)`;
+    const sqlQuery = `
+    INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
+    VALUES (?, ?, ?);`;
     await db.query(sqlQuery, [saleId, product.productId, product.quantity]);
+  },
+
+  getSales: async () => {
+    const sqlQuery = `
+    select sp.sale_id as saleId,
+    s.date,
+    sp.product_id as productId,
+    sp.quantity
+    from sales_products as sp
+    join sales as s
+    on s.id=sp.sale_id;`;
+    const [sales] = await db.query(sqlQuery);
+    return sales;
+  },
+
+  getSale: async (id) => {
+    const sqlQuery = `
+    select s.date,
+    sp.product_id as productId,
+    sp.quantity
+    from sales_products as sp
+    join sales as s
+    on s.id=sp.sale_id
+    where s.id = ?;`;
+    const [sale] = await db.query(sqlQuery, [id]);
+    return sale;
   },
 };
 
