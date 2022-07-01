783ec943ed5493469aa49a83cd1bb9c53d83514c
Jhon Santana <jhonsstn@gmail.com>
2022-06-30 22:30:31 -0300

    fix(ProductValidator): fix name length logic

--- middlewares/product-validator.js
+++ middlewares/product-validator.js
@@ -1,11 +1,11 @@
 const Boom = require('@hapi/boom');
 
 const ProductValidator = {
-  validateProduct(product) {
+  validateProduct: (product) => {
     if (!product.name) {
       throw Boom.badRequest('"name" is required');
     }
-    if (!product.name.length < 5) {
+    if (product.name.length < 5) {
       throw Boom.badData('"name" length must be at least 5 characters long');
     }
   },
