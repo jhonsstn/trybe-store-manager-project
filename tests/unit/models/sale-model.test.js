776a3577358ebb5b50bff46130cd73be8a30e4fd
Jhon Santana <jhonsstn@gmail.com>
2022-07-01 20:00:15 -0300

    test(SaleModel): ensure returns sales list if calls getSale/getSales

--- tests/unit/models/sale-model.test.js
+++ tests/unit/models/sale-model.test.js
@@ -14,4 +14,47 @@ describe('SaleModel', () => {
 
     expect(result).to.equal(1);
   });
+
+  it('should create a product sale if calls createProductSales', async () => {
+    const queryStub = sinon.stub(db, 'query').resolves();
+    await SaleModel.createProductSales(1, {
+      productId: 1,
+      quantity: 1,
+    });
+
+    expect(queryStub.calledOnce).to.be.true;
+  });
+
+  it('should get all sales if calls getSales', async () => {
+    const sales = [
+      {
+        id: 1,
+        productId: 1,
+        quantity: 1,
+      },
+      {
+        id: 2,
+        productId: 2,
+        quantity: 5,
+      },
+    ];
+
+    sinon.stub(db, 'query').resolves([sales]);
+    const result = await SaleModel.getSales();
+
+    expect(result).to.deep.equal(sales);
+  });
+
+  it('should get a sale if calls getSale', async () => {
+    const sale = {
+      id: 1,
+      productId: 1,
+      quantity: 1,
+    };
+
+    sinon.stub(db, 'query').resolves([sale]);
+    const result = await SaleModel.getSale(1);
+
+    expect(result).to.deep.equal(sale);
+  });
 });
